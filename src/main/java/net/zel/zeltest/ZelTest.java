86c5a272f703f92f304c42f9859ba3f16b3f2435
Supertf2 <146997522+Supertf2@users.noreply.github.com>
2025-06-09 01:50:43 +0300

    Initial commit

---
+++ src/main/java/net/zel/zeltest/ZelTest.java
@@ -0,0 +1,16 @@
+package net.zel.zeltest;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZelTest implements ModInitializer {
+	public static final String MOD_ID = "zel-test";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zel/zeltest/zeltestclient.java
@@ -0,0 +1,10 @@
+package net.zel.zeltest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class zeltestclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
