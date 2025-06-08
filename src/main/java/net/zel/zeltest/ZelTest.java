ad25593fc52f6f246412dc3a4815fd45805475c2
Supertf2 <146997522+Supertf2@users.noreply.github.com>
2025-06-09 02:55:11 +0300

    a

--- src/main/java/net/zel/zeltest/ZelTest.java
+++ src/main/java/net/zel/zeltest/ZelTest.java
@@ -2,6 +2,7 @@ package net.zel.zeltest;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.zel.zeltest.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class ZelTest implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/zel/zeltest/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.zel.zeltest.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.zel.zeltest.ZelTest;
+
+public class ModItems {
+        public static final Item MOTHER_PLATE = registerItem(name: "mother_plate", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ZelTest.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        ZelTest.LOGGER.info("Registering Mod Items for " + ZelTest.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+
+            fabricItemGroupEntries.add(MOTHER_PLATE);
+        });
+    }
+
+}
