32e39fd0e4942fd54fd495b700b6a41b1d537628
Supertf2 <146997522+Supertf2@users.noreply.github.com>
2025-06-09 14:19:11 +0300

    items

--- src/main/java/net/zel/zeltest/item/ModItems.java
+++ src/main/java/net/zel/zeltest/item/ModItems.java
@@ -9,8 +9,8 @@ import net.minecraft.util.Identifier;
 import net.zel.zeltest.ZelTest;
 
 public class ModItems {
-        public static final Item MOTHER_PLATE = registerItem(name: "mother_plate", new Item(new Item.Settings()));
-
+        public static final Item MOTHER_PLATE = registerItem("mother_plate", new Item(new Item.Settings()));
+        public static final Item MINOS_PRIME = registerItem("minos_prime", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ZelTest.MOD_ID, name), item);
@@ -22,6 +22,7 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
 
             fabricItemGroupEntries.add(MOTHER_PLATE);
+            fabricItemGroupEntries.add(MINOS_PRIME);
         });
     }
 
